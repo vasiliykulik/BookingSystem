22d407ba8b733f55bc95ef2584b44e2f4d495d59
Konstantin <at0man@bk.ru>
2016-11-04 15:35:29 +0200

    add class Hotel, Room, User

---
+++ src/main/java/ua/goit/booking/hotel/Hotel.java
@@ -0,0 +1,67 @@
+package hotel;
+
+import java.util.List;
+
+public class Hotel {
+    private long id;
+    private String hotelName;
+    private String cityName;
+   // private RoomDAO roomDao;
+
+    public Hotel(long id, String hotelName, String cityName) {
+        this.id = id;
+        this.hotelName = hotelName;
+        this.cityName = cityName;
+        //this.roomDAO = new RoomDAO;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+   /* public RoomDAO getRoomDAO() {
+        return roomDAO;
+    }*/
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Hotel hotel = (Hotel) o;
+
+        if (id != hotel.id) return false;
+        if (hotelName != null ? !hotelName.equals(hotel.hotelName) : hotel.hotelName != null) return false;
+        return cityName != null ? cityName.equals(hotel.cityName) : hotel.cityName == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (hotelName != null ? hotelName.hashCode() : 0);
+        result = 31 * result + (cityName != null ? cityName.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/ua/goit/booking/users/User.java
@@ -0,0 +1,68 @@
+package users;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    public User(long id, String firstName, String lastName, String emailAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
+}
