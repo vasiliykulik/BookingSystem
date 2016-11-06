b2286b38e34642086f692e1db4d3e85e1501fbea
garfield <holyman.login@gmail.com>
2016-11-06 16:05:06 +0200

    getRooms...

--- src/main/java/ua/goit/booking/controller/RoomController.java
+++ src/main/java/ua/goit/booking/controller/RoomController.java
@@ -8,7 +8,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RoomController {
-    public List<Room> getAllFreeRooms() {
+    public List<Room> getAllRooms() {
+        List<Room> result = new ArrayList<>();
+        AbstractDao<Room> dao = new RoomDaoImpl();
+        result.addAll(dao.getAll());
+        return result;
+    }
+
+    public List<Room> getFreeRooms() {
         List<Room> result = new ArrayList<>();
         AbstractDao<Room> dao = new RoomDaoImpl();
         result.addAll(dao.getAll().stream()
@@ -20,6 +27,14 @@ public class RoomController {
         List<Room> result = new ArrayList<>();
         AbstractDao<Room> dao = new RoomDaoImpl();
         result.addAll(dao.getAll().stream()
+                .filter(room -> (room.getPrice() <= budget)).collect(Collectors.toList()));
+        return result;
+    }
+
+    public List<Room> getFreeCheaperRooms(int budget) {
+        List<Room> result = new ArrayList<>();
+        AbstractDao<Room> dao = new RoomDaoImpl();
+        result.addAll(dao.getAll().stream()
                 .filter(room -> (room.getPrice() <= budget) & (!room.isBooked())).collect(Collectors.toList()));
         return result;
     }
@@ -28,9 +43,24 @@ public class RoomController {
         List<Room> result = new ArrayList<>();
         AbstractDao<Room> dao = new RoomDaoImpl();
         result.addAll(dao.getAll().stream()
+                .filter(room -> (room.getNumberOfVisitors() == nPersons))
+                .collect(Collectors.toList()));
+        return result;
+    }
+
+    public List<Room> getFreeRoomsForNPersons(int nPersons) {
+        List<Room> result = new ArrayList<>();
+        AbstractDao<Room> dao = new RoomDaoImpl();
+        result.addAll(dao.getAll().stream()
                 .filter(room -> (room.getNumberOfVisitors() == nPersons) & (!room.isBooked()))
                 .collect(Collectors.toList()));
         return result;
     }
+
+//    public List<Room> getRoomsOfTheCity(String theCity) {
+//        List<Room> result = new ArrayList<>();
+//        //...
+//        return result;
+//    }
 
 }
