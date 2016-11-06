f97dea97469411c73d58d37f61e5c1b30af140ce
garfield <holyman.login@gmail.com>
2016-11-06 15:54:00 +0200

    getCheaperRooms

--- src/main/java/ua/goit/booking/controller/RoomController.java
+++ src/main/java/ua/goit/booking/controller/RoomController.java
@@ -15,10 +15,13 @@ public class RoomController {
         return result;
     }
 
-    public List<Room> getAllCheaperRooms(int budget) {
+    public List<Room> getCheaperRooms(int budget) {
         List<Room> result = new ArrayList<>();
         AbstractDao<Room> dao = new RoomDaoImpl();
-        result.addAll(dao.getAll().stream().filter(room -> room.getPrice() <= budget).collect(Collectors.toList()));
+        result.addAll(dao.getAll().stream()
+                .filter(room -> (room.getPrice() <= budget)&(!room.isBooked())).collect(Collectors.toList()));
         return result;
     }
+
+
 }
