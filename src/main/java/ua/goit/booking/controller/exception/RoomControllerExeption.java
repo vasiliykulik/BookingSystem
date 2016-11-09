38801f17048ee24f29e0016c446a4bae95103fe6
TarasLavrenyuk <lavrik1997@gmail.com>
2016-11-10 00:26:33 +0200

    Finally. Bug fixed

---
+++ src/main/java/ua/goit/booking/controller/exception/RoomControllerExeption.java
@@ -0,0 +1,7 @@
+package ua.goit.booking.controller.exception;
+
+public class RoomControllerExeption extends Exception{
+    public RoomControllerExeption(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ua/goit/booking/dao/LocaleDate.java
@@ -0,0 +1,7 @@
+package ua.goit.booking.dao;
+
+/**
+ * Created by taras on 09.11.16.
+ */
+public class LocaleDate {
+}
