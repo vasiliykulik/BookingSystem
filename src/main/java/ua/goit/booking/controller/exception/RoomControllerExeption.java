f3e86119efeed54f3fdaa5492e4ca183c1ce61ed
Dmytro Lisovyi <dimalis.mail@gmail.com>
2016-11-10 20:06:09 +0200

    remove artifakts

--- src/main/java/ua/goit/booking/controller/exception/RoomControllerExeption.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.booking.controller.exception;
-
-public class RoomControllerExeption extends Exception{
-    public RoomControllerExeption(String message) {
-        super(message);
-    }
-}
--- src/main/java/ua/goit/booking/dao/LocaleDate.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.booking.dao;
-
-/**
- * Created by taras on 09.11.16.
- */
-public class LocaleDate {
-}
